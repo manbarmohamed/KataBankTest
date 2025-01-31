2e8c3b7b0cce1fa6dc5cb86c2fd0d7ef644132bb
manbarmohamed <manbar.mohammed@gmail.com>
2025-01-31 17:02:34 +0100

    Create Transaction class with date, amount, and balance

--- src/main/java/org/bank/Transaction.java
+++ src/main/java/org/bank/Transaction.java
@@ -1,4 +1,22 @@
 package org.bank;
 
 public class Transaction {
+
+    private final String date;
+    private final int amount;
+    private final int balance;
+
+    public Transaction(String date, int amount, int balance) {
+        this.date = date;
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+    public String getFormattedTransaction() {
+        return String.format("%s | %d | %d", date, amount, balance);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
