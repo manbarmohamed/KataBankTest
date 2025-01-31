430002a57a5f29d1f34bf66ff56ad86160fcc7c8
manbarmohamed <manbar.mohammed@gmail.com>
2025-01-31 17:05:59 +0100

    Implement Account class with deposit and withdraw methods

--- src/main/java/org/bank/Account.java
+++ src/main/java/org/bank/Account.java
@@ -1,4 +1,29 @@
 package org.bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+
+    private List<Transaction> transactions = new ArrayList<>();
+    private int balance = 0;
+    private StatementPrinter printer;
+
+    public Account(StatementPrinter printer) {
+        this.printer = printer;
+    }
+
+    public void deposit(int amount, String date) {
+        balance += amount;
+        transactions.add(new Transaction(date, amount, balance));
+    }
+
+    public void withdraw(int amount, String date) {
+        balance -= amount;
+        transactions.add(new Transaction(date, -amount, balance));
+    }
+
+    public void printStatement() {
+        printer.print(transactions);
+    }
 }
